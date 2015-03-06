1b01610c2fa8900a7039ceb4b0d3641ccbce26b8
gmihai <mihai.giurgeanu@gmail.com>
2015-03-06 03:49:47 +0000

    Project genearted with leiningen app template.

---
+++ src/mentionmyfollowers/core.clj
@@ -0,0 +1,7 @@
+(ns mentionmyfollowers.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/mentionmyfollowers/core_test.clj
@@ -0,0 +1,7 @@
+(ns mentionmyfollowers.core-test
+  (:require [clojure.test :refer :all]
+            [mentionmyfollowers.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
