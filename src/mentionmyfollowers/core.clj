ed9816dc63d52d23aa52c0dcf6ede686f40a477c
gmihai <mihai.giurgeanu@gmail.com>
2015-03-07 07:27:00 +0000

    Added route for error case in app authorization.

--- src/mentionmyfollowers/core.clj
+++ src/mentionmyfollowers/core.clj
@@ -5,7 +5,11 @@
 
 (defroutes api-routes
     (GET "/api/followers" [u p] (str "request followers for " u " with pass " p))
-    (GET "/session" [code session] (str "Received code " code " for session " session))
+    (GET "/session" [session code] 
+         (when code (str "Received code " code " for session " session "\n")))
+    (GET "/session" [session error error_reason error_description] 
+         (when error (str "Received error " error "/" error_reason "/" error_description " for session " session "\n")))
+    (GET "/session" req (str "Unkonwn session request params: " (:params req)))
     (route/not-found "Service not found"))
 
 (def api (wrap-defaults api-routes api-defaults))
